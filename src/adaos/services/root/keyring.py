5426d4a88968251d07a886b4c3e121034e2ef5d0
stipot <93196124+stipot@users.noreply.github.com>
2025-09-21 21:23:27 +0300

    fix: depts in services

--- src/adaos/services/root/keyring.py
+++ src/adaos/services/root/keyring.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from datetime import datetime
 import hashlib, base64, os
 
-from adaos.sdk.data.context import get_ctx
+from adaos.services.agent_context import get_ctx
 from adaos.ports.root import PubKey, KeypairRef
 
 
